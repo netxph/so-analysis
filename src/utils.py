3ade7083de0e1ec396ee366650dd34286859aa4e
Marc Vitalis <marc.vitalis@live.com>
2021-07-25 00:55:43 +0800

    more investigation

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,11 @@ def to_snake_case(name):
     name = re.sub('__([A-Z])', r'_\1', name)
     name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name)
     return name.lower()
+
+def count_percentage(x, y, data):
+    return data \
+        .groupby(x)[y] \
+        .value_counts(normalize=True) \
+        .mul(100) \
+        .rename('percent') \
+        .reset_index()
